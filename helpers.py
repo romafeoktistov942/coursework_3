e26da3f5ac73cc77e114f91b5e9b7e794e24e00f
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-06-07 17:02:09 +0300

    add function for masking card number

--- helpers.py
+++ helpers.py
@@ -10,3 +10,20 @@ def mask_account(account_number: str) -> str:
     """
     masked_account = "**" + account_number[-4:]
     return masked_account
+
+
+def mask_card(card_number: str) -> str:
+    """
+    Masks a card number, revealing only the first 6 and last 4 digits.
+
+    Args:
+        card_number (str): The card number to be masked.
+
+    Returns:
+        str: The masked card number, formatted in groups of 4 digits separated by spaces.
+    """
+    masked_number = card_number[:6] + "*" * 6 + card_number[-4:]
+    masked_number = " ".join(
+        [masked_number[i : i + 4] for i in range(0, len(masked_number), 4)]
+    )
+    return masked_number
